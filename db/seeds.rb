7b58360fb7e8fe69b204c6371725183aa3b54893
ruchi ramani <ruchiramani@gmail.com>
2016-12-09 11:40:25 -0500

    added post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+p = Post.new(title: "My first post!", author: "Ruchi")
+p.contents = "hello"
+p.save
+p = Post.new(title: "My second port!", author: "Ruchi")
+p.contents = "hello for the second time!"
+p.save
