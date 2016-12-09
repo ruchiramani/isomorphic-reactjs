fbdba9dc8ec3e712df049ceaf302c0ae20fda1cb
ruchi ramani <ruchiramani@gmail.com>
2016-12-09 12:19:13 -0500

    added react

---
+++ app/assets/javascripts/components/posts.js.jsx
@@ -0,0 +1,33 @@
+var Post = React.createClass({
+  render: function() {
+    return (
+      <div className="post">
+       <PostHeader post={this.props.post} />
+       </div>
+    );
+  }
+});
+
+var PostHeader = React.createClass({
+  render: function(){
+    return (
+      <div className="post-header">
+       <h2> {this.props.post.title} </h2>
+       <div className="post-meta">
+        By {this.props.post.author} - {this.props.post.created_at}
+        </div>
+        </div>
+
+    );
+  }
+});
+
+var PostContent = React.createClass({
+  render: function(){
+    return (
+      <div className="post-contents">
+      {this.props.post.contents}
+      </div>
+    );
+  }
+});
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,3 @@ class PostsController < ApplicationController
   end
 
 end
- 
